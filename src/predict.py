5034e169b4bbc08a5c2ed30057d9db7f883cf0c6
faventur <faventur@student.42mulhouse.fr>
2024-07-21 17:50:10 +0200

    The model works!

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,5 @@
 def estimatePrice(mileage, θ0, θ1):
-    return round(θ0 + (θ1 * mileage), 2)
+    return θ0 + (θ1 * mileage)
 
 
 def main():
@@ -12,7 +12,7 @@ def main():
             print(f'{type(error).__name__}: {error}')
 
         mileage = input("Enter your mileage: ")
-        estimatedPrice = estimatePrice(float(mileage), float(θ0), float(θ1))
+        estimatedPrice = round(estimatePrice(float(mileage), float(θ0), float(θ1)), 2)
         print(estimatedPrice if estimatedPrice > 0 else 0)
     except Exception as error:
         print(f'{type(error).__name__}: {error}')
--- src/train.py
+++ src/train.py
@@ -5,35 +5,59 @@ from load_csv import load
 from predict import estimatePrice
 
 
+def gradient_descent(X, Y, θ0, θ1, learningRate, iterations):
+    m = len(X)  # Number of training examples
+
+    for _ in range(iterations):
+        sum_tmpθ0 = 0.0
+        sum_tmpθ1 = 0.0
+
+        for i in range(m):
+            error = estimatePrice(X[i], θ0, θ1) - Y[i]
+            sum_tmpθ0 += error
+            sum_tmpθ1 += error * X[i]
+
+        tmpθ0 = learningRate * (1/m) * sum_tmpθ0
+        tmpθ1 = learningRate * (1/m) * sum_tmpθ1
+
+        θ0 -= tmpθ0
+        θ1 -= tmpθ1
+
+    return θ0, θ1
+
+
 def main():
     # collecting x & y
     df = load("../material/data.csv")
     X = df['km']
     Y = df['price']
 
-    # calculate mean of x & y using an inbuilt numpy method mean()
-    mean_x = np.mean(X)
-    mean_y = np.mean(Y)
-
-    # total number of input values
-    m = len(X)
+    normalizedX = (X - X.mean()) / X.std()
+    normalizedY = (Y - Y.mean()) / Y.std()
 
     # using the formula to calculate θ0 & θ1
-    num = 0
-    den = 0
-    for i in range(m):
-        num += (X[i] - mean_x) * (Y[i] - mean_y)
-        den += (X[i] - mean_x) ** 2
-    θ1 = num / den
-    θ0 = mean_y - (θ1 * mean_x)
+    θ0 = θ1 = 1
+    learningRate = 0.01
+    iterations = 100000
+    
+    θ0_norm, θ1_norm = gradient_descent(normalizedX, normalizedY, θ0, θ1,
+                                        learningRate, iterations)
+
+    # Transforming θ0 and θ1 back to original scale
+    θ1 = θ1_norm * (Y.std() / X.std())
+    θ0 = θ0_norm * Y.std() + Y.mean() - θ1 * X.mean()
+
+    print(f"theta0: {θ0}, theta1: {θ1}")
 
     # plotting values and regression line
     max_x = np.max(X) + 100
-    min_x = np.min(Y) - 100
+    min_x = np.min(X) - 100
 
     # calculating line values x and y
     x = np.linspace(min_x, max_x, 100)
+    print(x)
     y = θ0 + θ1 * x
+    print(f"theta0: {θ0}, theta1: {θ1}")
 
     plt.plot(x, y, color='#58b970', label='Regression Line')
     plt.scatter(X, Y, c='#ef5423', label='data points')
@@ -42,6 +66,10 @@ def main():
     plt.ylabel('Mileage')
     plt.legend()
     plt.show()
+
+    # calculate mean of x & y using an inbuilt numpy method mean()
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
 
     # calculating R-squared value for measuring goodness of our model
 
@@ -57,7 +85,7 @@ def main():
     print(r2)
 
     try:
-        with open("../material/θ.csv", "tw") as file:
+        with open("../material/θ.csv", "w") as file:
             file.write(f'{ θ0 },{ θ1 }')
     except Exception as error:
         print(f'{type(error).__name__}: {error}')
